ffdd4157e4456b6c509d8321db8afdf1d1222e49
arfat-xyz <arfatrahman08@gmail.com>
2023-07-23 19:42:56 +0600

    api slice added

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,9 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import cartSlice from './features/cart/cartSlice';
 import productSlice from './features/products/productSlice';
+import { api } from './features/api/apiSlice';
 export const store = configureStore({
-  reducer: { cart: cartSlice, product: productSlice },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  reducer: {
+    cart: cartSlice,
+    product: productSlice,
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(logger, api.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
